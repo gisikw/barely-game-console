4d8f15f042dca3df25d4ca823c274a06b7774041
= <kevin@kevingisi.com>
2024-11-28 06:59:50 +0000

    Core functionality working, much cleanup and refactoring needed

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,14 @@ mod rom_preview;
 mod ui;
 
 use crate::app::BarelyGameConsole;
-use crate::config::Config;
+use crate::config::{CardInfo, Config};
 use crate::rfid_reader::RFIDReader;
 use eframe::egui;
+use evdev::{Device, InputEventKind, Key};
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid;
+use std::process::Command;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -30,8 +35,7 @@ impl Coordinator {
         let config = Config::load();
 
         let ui_app = Arc::new(Mutex::new(ui_app));
-        automated_test(config.clone(), Arc::clone(&ui_app));
-        rfid_listener(config.clone(), Arc::clone(&ui_app));
+        device_listener(config.clone(), Arc::clone(&ui_app));
 
         Self { ui_app }
     }
@@ -45,52 +49,110 @@ impl eframe::App for Coordinator {
     }
 }
 
-fn automated_test(config: Config, ui_app: Arc<Mutex<BarelyGameConsole>>) {
-    thread::spawn(move || {
-        thread::sleep(Duration::from_secs(3));
-        if let Ok(mut app) = ui_app.lock() {
-            app.enqueue_rom(Some(
-                config.rfid_cards.get("1234567890").unwrap().artwork.clone(),
-            ));
-        }
+// Overloaded to listen to rfid AND power
+fn device_listener(config: Config, ui_app: Arc<Mutex<BarelyGameConsole>>) {
+    let timer_version = Arc::new(AtomicUsize::new(0));
+    let selected_rom: Arc<Mutex<Option<CardInfo>>> = Arc::new(Mutex::new(None));
+    let game_pid: Arc<Mutex<Option<u32>>> = Arc::new(Mutex::new(None));
 
-        thread::sleep(Duration::from_secs(5));
-        if let Ok(mut app) = ui_app.lock() {
-            app.enqueue_rom(Some(
-                config.rfid_cards.get("0987654321").unwrap().artwork.clone(),
-            ));
-        }
+    thread::spawn({
+        let game_pid = Arc::clone(&game_pid);
+        let selected_rom = Arc::clone(&selected_rom);
+        let timer_version = Arc::clone(&timer_version);
+        let ui_app = Arc::clone(&ui_app);
+        move || {
+            let device_path = "/dev/input/event2";
+            let mut device = Device::open(device_path).expect("Failed to open the button");
+            let _ = device.grab();
 
-        thread::sleep(Duration::from_secs(5));
-        if let Ok(mut app) = ui_app.lock() {
-            app.enqueue_rom(None);
-        }
+            loop {
+                for event in device.fetch_events().expect("could not fetch event") {
+                    if event.value() == 0 && event.kind() == InputEventKind::Key(Key::KEY_POWER) {
+                        if let Ok(pid) = game_pid.lock() {
+                            if let Some(pid) = *pid {
+                                let _ = kill(Pid::from_raw(pid as i32), Signal::SIGKILL);
+                            } else {
+                                if let Ok(mut selected_rom) = selected_rom.lock() {
+                                    let rom = (&*selected_rom).clone();
+                                    if let Some(rom) = rom {
+                                        *selected_rom = None;
+                                        timer_version.fetch_add(1, Ordering::SeqCst);
+                                        if let Ok(mut app) = ui_app.lock() {
+                                            app.enqueue_rom(None);
+                                        }
 
-        thread::sleep(Duration::from_secs(5));
-        if let Ok(mut app) = ui_app.lock() {
-            app.enqueue_rom(None);
-        }
-
-        thread::sleep(Duration::from_secs(3));
-        if let Ok(mut app) = ui_app.lock() {
-            app.enqueue_rom(Some(
-                config.rfid_cards.get("1234567890").unwrap().artwork.clone(),
-            ));
+                                        thread::spawn({
+                                            let game_pid = Arc::clone(&game_pid);
+                                            move || {
+                                                if let Ok(mut child) = Command::new("retroarch")
+                                                    .arg("-L")
+                                                    .arg(rom.emulator)
+                                                    .arg(rom.rom_path)
+                                                    .arg("--appendconfig")
+                                                    .arg("retroarch.cfg")
+                                                    .spawn()
+                                                {
+                                                    if let Ok(mut pid) = game_pid.lock() {
+                                                        *pid = Some(child.id());
+                                                    }
+                                                    let _ = child.wait();
+                                                    if let Ok(mut pid) = game_pid.lock() {
+                                                        *pid = None;
+                                                    }
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     });
-}
 
-fn rfid_listener(config: Config, ui_app: Arc<Mutex<BarelyGameConsole>>) {
-    thread::spawn(move || {
-        let reader = RFIDReader::new();
-        reader.run(|id| {
-            if let Ok(mut app) = ui_app.lock() {
-                app.enqueue_rom(Some(id));
-            }
-            thread::sleep(Duration::from_secs(5));
-            if let Ok(mut app) = ui_app.lock() {
-                app.enqueue_rom(None);
-            }
-        });
+    thread::spawn({
+        let game_pid = Arc::clone(&game_pid);
+        let selected_rom = Arc::clone(&selected_rom);
+        let timer_version = Arc::clone(&timer_version);
+        let ui_app = Arc::clone(&ui_app);
+        move || {
+            let reader = RFIDReader::new();
+            reader.run(move |id| {
+                if let Ok(pid) = game_pid.lock() {
+                    if (*pid).is_none() {
+                        if let Some(rom) = config.rfid_cards.get(&id) {
+                            if let Ok(mut sel_rom) = selected_rom.lock() {
+                                *sel_rom = Some(rom.clone());
+
+                                let current_version =
+                                    timer_version.fetch_add(1, Ordering::SeqCst) + 1;
+                                if let Ok(mut app) = ui_app.lock() {
+                                    app.enqueue_rom(Some(rom.artwork.clone()));
+                                }
+
+                                let selected_rom = Arc::clone(&selected_rom);
+                                let timer_version = Arc::clone(&timer_version);
+                                let ui_app = Arc::clone(&ui_app);
+                                thread::spawn(move || {
+                                    thread::sleep(Duration::from_secs(5));
+                                    if timer_version.load(Ordering::SeqCst) == current_version {
+                                        if let Ok(mut selected_rom) = selected_rom.lock() {
+                                            *selected_rom = None;
+                                        }
+                                        if let Ok(mut app) = ui_app.lock() {
+                                            app.enqueue_rom(None);
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            println!("Unknown id: {:?}", id);
+                        }
+                    }
+                }
+            });
+        }
     });
 }
--- src/rfid_reader.rs
+++ src/rfid_reader.rs
@@ -16,11 +16,48 @@ impl RFIDReader {
 
         let _ = device.grab();
 
+        let mut id = String::new();
         loop {
             for event in device.fetch_events().expect("could not fetch event") {
                 if let InputEventKind::Key(key) = event.kind() {
-                    if key == Key::KEY_ENTER && event.value() == 0 {
-                        on_id("Super Mario World".to_string());
+                    if event.value() == 0 {
+                        match key {
+                            Key::KEY_ENTER => {
+                                on_id(id.clone());
+                                id.clear();
+                            }
+                            Key::KEY_1 => {
+                                id.push('1');
+                            }
+                            Key::KEY_2 => {
+                                id.push('2');
+                            }
+                            Key::KEY_3 => {
+                                id.push('3');
+                            }
+                            Key::KEY_4 => {
+                                id.push('4');
+                            }
+                            Key::KEY_5 => {
+                                id.push('5');
+                            }
+                            Key::KEY_6 => {
+                                id.push('6');
+                            }
+                            Key::KEY_7 => {
+                                id.push('7');
+                            }
+                            Key::KEY_8 => {
+                                id.push('8');
+                            }
+                            Key::KEY_9 => {
+                                id.push('9');
+                            }
+                            Key::KEY_0 => {
+                                id.push('0');
+                            }
+                            _ => {}
+                        }
                     }
                 }
             }
